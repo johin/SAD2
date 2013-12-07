1223bf6f1611d4556af92fe979c12d1a0ccad954
Jesper Jensen <jj@nertconn.dk>
2013-12-07 23:19:41 +0100

    SQL Check Script

---
+++ jesper/Clique_CheckRoutine.sql
@@ -0,0 +1,66 @@
+delimiter $$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Check4Clique`(
+	IN p_actor1 INT(10), 
+	IN p_actor2 INT(10),
+	IN p_actor3 INT(10),
+	IN p_actor4 INT(10),
+	OUT p_count INT(10))
+BEGIN
+-- Decalre working variables
+DECLARE cnt, tmpVal INT default 0;
+DECLARE done INT DEFAULT FALSE;
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+-- Create temp tabels for results 
+ DROP TEMPORARY TABLE if exists TmpActor1;
+ CREATE TEMPORARY TABLE TmpActor1 engine=memory (
+ 	SELECT actor_id, movie_id from imdb.roles where actor_id = p_actor1
+ );
+ DROP TEMPORARY TABLE if exists TmpActor2;
+ CREATE TEMPORARY TABLE TmpActor2 engine=memory (
+ 	SELECT actor_id, movie_id from imdb.roles where actor_id = p_actor2
+ );
+ DROP TEMPORARY TABLE if exists TmpActor3;
+ CREATE TEMPORARY TABLE TmpActor3 engine=memory (
+ 	SELECT actor_id, movie_id from imdb.roles where actor_id = p_actor3
+ );
+ DROP  TABLE if exists TmpActor4;
+ CREATE TEMPORARY TABLE TmpActor4 engine=memory (
+ 	SELECT actor_id, movie_id from imdb.roles where actor_id = p_actor4
+ );
+
+DROP  TABLE if exists CmpTbl1;
+ CREATE TEMPORARY TABLE CmpTbl1 engine=memory (
+ 	SELECT TmpActor1.movie_id
+	FROM TmpActor1 INNER JOIN TmpActor2
+	USING (movie_id)
+ );
+
+DROP  TABLE if exists CmpTbl2;
+ CREATE TEMPORARY TABLE CmpTbl2 engine=memory (
+ 	SELECT TmpActor3.movie_id
+	FROM TmpActor3 INNER JOIN TmpActor4
+	USING (movie_id)
+ );
+DROP  TABLE if exists CliqueTbl;
+ CREATE TEMPORARY TABLE CliqueTbl engine=memory (
+ 	SELECT CmpTbl1.movie_id
+	FROM CmpTbl1 INNER JOIN CmpTbl2
+	USING (movie_id)
+ );
+
+-- Clean up in memory
+DROP TEMPORARY TABLE TmpActor1;
+DROP TEMPORARY TABLE TmpActor2;
+DROP TEMPORARY TABLE TmpActor3;
+DROP TEMPORARY TABLE TmpActor4;
+
+SELECT count(movie_id) INTO p_count FROM CliqueTbl;
+
+-- Drop temp tables on exit
+-- DROP TEMPORARY TABLE CmpTbl1;
+-- DROP TEMPORARY TABLE CmpTbl2;
+-- DROP TEMPORARY TABLE CliqueTbl;
+END$$
+
