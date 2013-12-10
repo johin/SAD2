3d814d6d4c32daa9fd1226ad1bc71055a90861f5
Jonas Hinge <jonas.hinge@gmail.com>
2013-12-10 16:38:15 +0100

    final (maybe) 3 algorithms - naive, optimised, optimal

--- jonas/max_weighted_4clique.py
+++ jonas/max_weighted_4clique.py
@@ -2,8 +2,9 @@ import pickle
 import sys
 import MySQLdb as mdb
 from pprint import pprint
+import time
 
-USE_CACHE = True
+USE_CACHE = False
 
 SQL_GET_ALL_MOVIES = """
 	SELECT id FROM movies
@@ -20,6 +21,7 @@ SQL_GET_ROLES_OF_MOVIE = """
 
 def build_graph():
 	adj = {}
+	edges = 0
 	con = mdb.connect('127.0.0.1', 'root', 'geekchamp3!', 'imdb')
 	with con:
 		cur = con.cursor()
@@ -44,67 +46,88 @@ def build_graph():
 							else:
 								adj[actor][actor_other] = 1
 						else:
-							adj[actor] = {}
-	return adj
+							adj[actor] = {actor_other: 1}
+						edges += 1
+	return adj, edges/2
 
 
 def naive_max_edgeweighted_4clique(G):
 	current_clique = ((),-1)
+	cliques_total = {}
 	for v, v_neighbours in G.iteritems():
 		for u, u_wt in v_neighbours.iteritems():
 			for w, w_wt in v_neighbours.iteritems():
 				if(w in G[u]):
 					for z, z_wt in v_neighbours.iteritems():
 						if(z in G[w] and z in G[u] and z != w and z != u):
+							update_discovered(cliques_total,v,u,w,z,get_clique_weight(G,v,u,w,z))
 							if(get_clique_weight(G,v,u,w,z) > current_clique[1]):
 								current_clique = ((v,u,w,z),get_clique_weight(G,v,u,w,z))
-								print "Found clique: " + "v: " + v + ", u: " + u + ", w: " + w + ", z: " + z
+								print "Found a clique higher: " + "v: " + v + ", u: " + u + ", w: " + w + ", z: " + z
 								print "Weight of clique: " + str(get_clique_weight(G,v,u,w,z))
-	return current_clique
+	return cliques_total
 
 
 def optimized_max_edgeweighted_4clique(G):
 	current_clique = ((),-1)
-	tri_processed = {}
+	cliques_total = {}
 	print "Counting triangles..."
-	triangles = node_iterator_plus_plus(G);
+	triangles, cliques = node_iterator_plus_plus_w_ids(G);
+	#triangles, cliques = node_iterator(G);
 	print "Finished counting triangles"
-	#print len(triangles)
 	for v,u,w in triangles:
-		tri = int(v.split("_")[0]) + int(u.split("_")[0]) + int(w.split("_")[0])
-		if(tri in tri_processed):
-			#print "triangle already processed: " + str(tri)
-			continue
 		v_adj,u_adj,w_adj = G[v].keys(),G[u].keys(),G[w].keys()
-		#union_set = (v_adj.union(u_adj)).union(w_adj)
-		#union_list = intersect(intersect(v_adj,u_adj),w_adj)
-		#union_list = intersect(v_adj, u_adj, w_adj)
-		union_list = intersect(G, v_adj, u, w)
-		#print union_list
-		if(len(union_list) > 0):
-			for z in union_list:
+		intersection_list = intersect(G, v_adj, u, w)
+		if(len(intersection_list) > 0):
+			for z in intersection_list:
 				weight = get_clique_weight(G,v,u,w,z)
-				if(weight >= current_clique[1]):
-					print "Found clique: " + "v: " + v + ", u: " + u + ", w: " + w + ", z: " + z
+				update_discovered(cliques_total,v,u,w,z,weight)
+				if(weight > current_clique[1]):
+					print "Found a clique higher: " + "v: " + v + ", u: " + u + ", w: " + w + ", z: " + z
 					print "Weight of clique: " + str(weight)
 					current_clique = ((v,u,w,z),weight)
-				# update aux
-				tri_one = int(v.split("_")[0]) + int(u.split("_")[0]) + int(w.split("_")[0]) #v+u+w
-				tri_two = int(v.split("_")[0]) + int(u.split("_")[0]) + int(z.split("_")[0]) #v+u+z
-				tri_three = int(v.split("_")[0]) + int(w.split("_")[0]) + int(z.split("_")[0]) #v+w+z
-				tri_four = int(u.split("_")[0]) + int(w.split("_")[0]) + int(z.split("_")[0]) #u+w+z
-				
-				tri_processed[tri_one] = 1
-				tri_processed[tri_two] = 1
-				tri_processed[tri_three] = 1
-				tri_processed[tri_four] = 1
-	return current_clique
+	return cliques_total
 
+
+def node_iterator_plus_plus_extension_4cliques(G):
+	current_clique = ((),-1)
+	cliques_total = {}
+	for v, v_neighbours in G.iteritems():
+		for u, u_wt in v_neighbours.iteritems():
+			if(u.split("_")[0] > v.split("_")[0]):
+				for w, w_wt in v_neighbours.iteritems():
+					if(w.split("_")[0] > u.split("_")[0]):
+						if(w in G[u]):
+							for z, z_wt in v_neighbours.iteritems():
+								if(z.split("_")[0] > w.split("_")[0]):
+									if(z in G[w] and z in G[u]):
+										update_discovered(cliques_total,v,u,w,z,get_clique_weight(G,v,u,w,z))
+										if(get_clique_weight(G,v,u,w,z) > current_clique[1]):
+											current_clique = ((v,u,w,z),get_clique_weight(G,v,u,w,z))
+											print "Found a clique higher: " + "v: " + v + ", u: " + u + ", w: " + w + ", z: " + z
+											print "Weight of clique: " + str(get_clique_weight(G,v,u,w,z))
+	return cliques_total
 
 
 # TRIANGLE COUNTING
-def node_iterator_plus_plus(G):
+
+def node_iterator(G):
 	triangles = []
+	cliques_total = {}
+	for v, v_neighbours in G.iteritems():
+		for u, u_wt in v_neighbours.iteritems():
+			for w, w_wt in v_neighbours.iteritems():
+				if(w in G[u]):
+					triangles.append((v,u,w))
+					update_discovered(cliques_total,v,u,w,"0_0",-1)
+					#unique_key = int(v)+int(u)+int(w)
+					#triangles[unique_key] = 0
+
+	return triangles, cliques_total
+
+'''def node_iterator_plus_plus(G):
+	triangles = []
+	cliques_total = {}
 	for v, v_neighbours in G.iteritems():
 		for u, u_wt in v_neighbours.iteritems():
 			if(len(G[u]) >= len(v_neighbours)):
@@ -112,15 +135,35 @@ def node_iterator_plus_plus(G):
 					if(len(G[w]) >= len(G[u])):
 						if(w in G[u]):
 							triangles.append((v,u,w))
+							update_discovered(cliques_total,v,u,w,"0_0",-1)
 							#unique_key = int(v)+int(u)+int(w)
 							#triangles[unique_key] = 0
 
-	return triangles
+	return triangles, cliques_total'''
+
+def node_iterator_plus_plus_w_ids(G):
+	triangles = []
+	cliques_total = {}
+	for v, v_neighbours in G.iteritems():
+		for u, u_wt in v_neighbours.iteritems():
+			if(u.split("_")[0] > v.split("_")[0]):
+				for w, w_wt in v_neighbours.iteritems():
+					if(w.split("_")[0] > u.split("_")[0]):
+						if(w in G[u]):
+							triangles.append((v,u,w))
+							update_discovered(cliques_total,v,u,w,"0_0",-1)
+							#unique_key = int(v)+int(u)+int(w)
+							#triangles[unique_key] = 0
+
+	return triangles, cliques_total
 
 # HELPERS
 def get_clique_weight(adj,v,u,w,z):
-	return min(adj[v][u],adj[v][w],adj[v][z],adj[u][w],adj[u][z],adj[w][z])
-	#return adj[v][u] + adj[v][w] + adj[v][z] + adj[u][w] + adj[u][z] + adj[w][z]
+	# median
+	aux = [adj[v][u],adj[v][w],adj[v][z],adj[u][w],adj[u][z],adj[w][z]]
+	aux.sort()
+	return (aux[2]+aux[3]) / 2
+
 
 '''def intersect(a, b):
     """ return the intersection of two lists """
@@ -134,6 +177,16 @@ def intersect(adj, a, b_key, c_key):
     		intersection.append(i)
     return intersection
 
+def update_discovered(D,v,u,w,z,weight):
+	id_aux = [int(v.split("_")[0]), int(u.split("_")[0]), int(w.split("_")[0]), int(z.split("_")[0])]
+	id_aux.sort()
+	clique_key = str(id_aux[0]) + "_" + str(id_aux[1]) + "_" + str(id_aux[2]) + "_" + str(id_aux[3])
+	#clique_key = int(v.split("_")[0]) + int(u.split("_")[0]) + int(w.split("_")[0]) + int(z.split("_")[0])
+	if(clique_key in D):
+		D[clique_key] = (D[clique_key][0]+1, D[clique_key][1], D[clique_key][2])
+	else:
+		D[clique_key] = (1, weight, (v,u,w,z))
+
 
 
 # RUN
@@ -141,25 +194,50 @@ def intersect(adj, a, b_key, c_key):
 # build graph
 if not USE_CACHE:
 	print "Building graph from DB..."
-	graph = build_graph();
+	graph,edges = build_graph();
 	with open('cache.pickle', 'wb') as handle:
 		pickle.dump(graph, handle)
 else:
 	print "Building graph from cache..."
 	with open('cache.pickle', 'rb') as handle:
-		graph = pickle.load(handle)
+		graph,edges = pickle.load(handle)
 print "Finished building graph"
 #pprint(graph)
 
+#(2, -1, ('58423_Daniel_Brochu', '543807_Sonja_Ball', '123290_Bruce_Dinsmore', '0_0'))
+# test graph
+#pprint(graph["58423_Daniel_Brochu"])
+#pprint(graph["543807_Sonja_Ball"])
+
+print "n = " + str(len(graph))
+print "m = " + str(edges)
 
 print "Running algorithm..."
 
+start_time = time.time()
+
 # NAIVE
-#max_clique = naive_max_edgeweighted_4clique(graph)
-#pprint(max_clique)
+#cliques = naive_max_edgeweighted_4clique(graph)
 
 # OPTIMIZED
-optimized_max_edgeweighted_4clique(graph)
+#cliques = optimized_max_edgeweighted_4clique(graph)
+
+# OPTIMAL
+cliques = node_iterator_plus_plus_extension_4cliques(graph)
+
+print "Algorithm execution time: " + str(time.time() - start_time)
+
+# TESTING TRIANGLE COUNTING
+#triangles, cliques = node_iterator(graph)
+#triangles, cliques = node_iterator_plus_plus(graph)
+#triangles, cliques = node_iterator_plus_plus_w_ids(graph)
+
+
+'''for t,t_v in cliques.iteritems():
+	if(t_v[0] != 6):
+		print t_v'''
+#pprint(cliques)
+print "Number of cliques: " + str(len(cliques))
 
 
 
